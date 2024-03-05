74b8e7dd173cc5b0f58c05569033bf31c2b68964
Vasu Tanneru <vasutanneru035@gmail.com>
2024-03-05 22:42:44 +0530

    vasu-portfolio

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -138,13 +138,6 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
-    //   .then((result) => {
-    //     setOpen(true);
-    //     form.current.reset();
-    //   }, (error) => {
-    //     console.log(error.text);
-    //   });
   };
 
   return (
